8b34c3d2db1b51df721d1dc0be6d7bf1e478eade
fakhrulhidayat33 <71173025+fakhrulhidayat33@users.noreply.github.com>
2025-05-20 14:53:50 +0700

    Update AnyAll.py

--- Python/built-in/easy/AnyAll.py
+++ Python/built-in/easy/AnyAll.py
@@ -7,3 +7,5 @@ arr = list(input().split())
 if all([number[0] != "-" for number in arr]):
     if any([number == number[::-1] for number in arr]):
         print (True)
+    else: print(False)
+else: print(False)
