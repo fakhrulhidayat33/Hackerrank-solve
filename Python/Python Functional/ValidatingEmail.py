bf55b346e25fa51ff84217a1261054f3f129e0e8
fakhrulhidayat33 <71173025+fakhrulhidayat33@users.noreply.github.com>
2025-05-23 16:15:11 +0700

    Create ValidatingEmail.py
    I thought the domain of email only uses the alphabet, but I...

---
+++ Python/Python Functional/ValidatingEmail.py
@@ -0,0 +1,19 @@
+# Link: https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem?isFullScreen=true
+import re
+def fun(s):
+    # I only edit here
+    return re.search("^[_a-z\d-]+@[a-z]+\.[a-z]+$",s)
+    # until here
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
